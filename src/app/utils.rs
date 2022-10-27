c497e2e4a2680ca385f17a62370f69207742d7b2
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-10-27 22:56:13 +0200

    Moving code here and adding test boilerplate

--- src/app/utils.rs
+++ src/app/utils.rs
@@ -1,3 +1,30 @@
-pub fn get_arg(x: usize) -> Vec<String>{
-    vec![std::env::args().nth(x).expect("Failed when getting argument,")]
+use super::Action;
+
+pub fn get_args() -> Vec<String> {
+    let args: Vec<String> = std::env::args().collect();
+    args[1..].to_vec()
+}
+pub fn get_action(args: &Vec<String>) -> Action {
+    Action::from_string(args.first().unwrap()).expect("Argument error! No action.")
+}
+pub fn get_argument(args: &mut Vec<String>) -> Option<&str> {
+    match args.get_mut(1) {
+        Some(x) => Some(x),
+        None => {
+            panic!("Argument missing.")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn get_action_from_args() {
+        todo!()
+    }
+    #[test]
+    fn get_argument_from_args() {
+        todo!()
+    }
 }
