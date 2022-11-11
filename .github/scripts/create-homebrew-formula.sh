2142fe78c990429a5696aecdddb0fafe7005b18d
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-11-11 18:58:21 +0100

    updating action workflow

--- .github/scripts/create-homebrew-formula.sh
+++ .github/scripts/create-homebrew-formula.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd Formula
-version=$(sh ./get-version.sh)
+version=$(/bin/bash/ sh ./get-version.sh)
 brew create --rust "https://github.com/anthonwellsjo/dro/releases/tag/${version}"
