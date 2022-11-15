2a8b6b6dcdc5ed5ed1985d2d46163c3b71f00d37
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-11-15 08:28:45 +0100

    updating action workflow

--- .github/scripts/create-homebrew-formula.sh
+++ .github/scripts/create-homebrew-formula.sh
@@ -4,4 +4,4 @@ version="$(sh get-version.sh | cut -d "=" -f 2)"
 cd .github/scripts
 cd ../../Formula
 echo "Creating formula with this url: https://github.com/anthonwellsjo/dro/releases/tag/${version}"
-brew create --rust "https://github.com/anthonwellsjo/dro/releases/tag/${version} --set-version ${version} --set-license "MIT"" 
+brew create --rust "https://github.com/anthonwellsjo/dro/releases/tag/${version}" --set-version ${version} --set-license "MIT"
