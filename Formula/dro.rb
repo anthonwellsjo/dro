0e27b8400d886afed4101a35f269ed34838aa3a7
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-11-17 21:37:13 +0100

    add brew formula

---
+++ Formula/dro.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Dro < Formula
+  desc "Super minimal CLI todo (dro) application."
+  homepage "https://crates.io/crates/dro"
+  url "https://github.com/anthonwellsjo/dro/releases/tag/v0.1.5"
+  version "v0.1.5"
+  sha256 "f131c95f173e94e7526f4a8bad76f4ed6ef88cd4678895d337cf17c45a926be8"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", *std_cargo_args
+  end
+end
