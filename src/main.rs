de5f05e5ecc9f58886f9e17ef44066145830979b
anthonwellsjo <anthonwellsjo@gmail.com>
2022-12-24 19:10:38 +0100

    chore: fixing broken imports

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use app::bash_driver::display_action_response;
 mod app;
 
 fn main() {
-    let action = brr::get_argument_at(0).unwrap();
+    let action = arw_brr::get_argument_at(0).unwrap();
     let action = app::Action::from_string(&action);
-    let argument = brr::get_argument_at(1);
+    let argument = arw_brr::get_argument_at(1);
 
     let mut session = app::Session::new(); 
 
