21aac58f314a120af030f76d701eda660b636649
Carl Anthon Wellsjö <58119759+anthonwellsjo@users.noreply.github.com>
2022-12-27 22:03:16 +0100

    makes version dynamic and better filename (#10)

--- .github/scripts/create-homebrew-formula.sh
+++ .github/scripts/create-homebrew-formula.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
 cd .github/scripts
-version="$(sh get-version.sh | cut -d "=" -f 2)"
+version="$(./get-version.sh ../../Cargo.toml)"
 echo "this is the version ${version}";
 cd ../../Formula
 echo "Creating formula with this url: https://github.com/anthonwellsjo/dro/releases/tag/${version}"
